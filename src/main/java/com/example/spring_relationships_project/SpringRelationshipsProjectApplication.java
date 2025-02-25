49c5f089750955e9f9d4743dfd680ea89fe90be0
Fake4091 <aswends7517@gmail.com>
2025-02-25 13:12:25 -0600

    Initial Commit

---
+++ src/main/java/com/example/spring_relationships_project/SpringRelationshipsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_relationships_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRelationshipsProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRelationshipsProjectApplication.class, args);
+  }
+
+}
